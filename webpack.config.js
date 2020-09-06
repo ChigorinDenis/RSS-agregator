a11addfb848feaebeb3d48edd3230ebfd2bbf26b
ChigorinDenis <chigorin@bk.ru>
2020-09-06 14:31:02 +0500

    adding_badges

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin'; //// eslint-disable-line no-use-before-define
-export const entry = `${__dirname}/src/index.js`;
-export const output = {
-  path: `${__dirname}/dist`,
-  filename: 'bundle.js',
-};
-export const mode = process.env.NODE_ENV || 'development';
-export const module = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry:`${__dirname}/src/index.js`,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
       },
-    },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'template.html',
+    }),
   ],
+  devServer: {
+    contentBase: './src',
+    port: 8000,
+  },
 };
-export const plugins = [
-  new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: 'template.html',
-  }),
-];
-export const devServer = {
-  contentBase: './src',
-  port: 8000,
-};
+
